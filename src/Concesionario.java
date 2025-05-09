f0ce71e909f2ec48626fa4043eb9b2e68245243e
Juan Beltran <fernando.beltran@est.iudigital.edu.co>
2025-05-09 15:06:39 -0500

    se realiza cambio en la fila 26, agregando (inventario).

--- src/Concesionario.java
+++ src/Concesionario.java
@@ -15,7 +15,7 @@ public class Concesionario {
         Vehiculo vehiculoVendido = null;
         // Buscar el vehículo en el inventario
         for (Vehiculo v : inventario) {
-            if (v.getCodigo().equals(codigovehiculo)) {
+            if (v.getCodigo().equals(codigoVehiculo)) {
                 vehiculoVendido = v;
                 break;
             }
@@ -23,7 +23,7 @@ public class Concesionario {
         // Si se encuentra el vehículo, se registra la venta
         if (vehiculoVendido != null) {
             ventas.add(new Venta(monto, vehiculoVendido, apellido, nombre, documento));
-            *******.remove(vehiculoVendido);
+            inventario.remove(vehiculoVendido);
             System.out.println("Venta realizada: " + nombre + " " + apellido + " ha comprado " + vehiculoVendido.getMarca());
         } else {
             System.out.println("Error: Vehículo con código " + codigoVehiculo + " no encontrado.");
